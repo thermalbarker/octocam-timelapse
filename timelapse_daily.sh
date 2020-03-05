7fc6935ecc164f8857bb17375439a326c681f689
thermalbarker <thermalbarker@users.noreply.github.com>
2020-03-05 17:49:11 +0100

    Adding an hourly snapshot timelapse

--- timelapse_daily.sh
+++ timelapse_daily.sh
@@ -4,20 +4,27 @@ BASE_DIR=/data/output/Camera1
 OUTPUT_DIR=/data/output/timelapse
 DATE_DIR=$(date +'%Y-%m-%d')
 TODAY_DIR="${BASE_DIR}/${DATE_DIR}"
-
-YEAR_DIR="${OUTPUT_DIR}/$(date +'%Y')"
+YEAR=$(date +'%Y')
+YEAR_DIR="${OUTPUT_DIR}/${YEAR}"
 YEAR_TIMELAPSE=${YEAR_DIR}/stills
+
+HOURLY_DIR=${YEAR_DIR}/hourly
 
 TIMELAPSE_NAME=${DATE_DIR}-timelapse.avi
 TIMELAPSE_FILE="${YEAR_DIR}/${TIMELAPSE_NAME}"
+
+TIMELAPSE_YEAR=${YEAR_DIR}/${YEAR}-timelapse.avi
+TIMELAPSE_HOURLY=${YEAR_DIR}/${YEAR}-hourly-timelapse.avi
 
 echo $TODAY_DIR
 ls ${TODAY_DIR}/*.jpg | wc
 
 mkdir -p ${YEAR_DIR}
 mkdir -p ${YEAR_TIMELAPSE}
+mkdir -p ${HOURLY_DIR}
 chmod a+rx ${YEAR_DIR}
 chmod a+rx ${YEAR_TIMELAPSE}
+chmod a+rx ${HOURLY_DIR}
 
 # Calculate the files between sunrise and sunset
 python /data/output/scripts/sunrise.py > /tmp/suntimes.txt
@@ -38,12 +45,45 @@ else
 fi
 
 FILES=$(find $TODAY_DIR -name '*.jpg' $SUNRISE $SUNSET | sort)
-echo $FILES
+#echo $FILES
 
 # Copy the snapshot from midday
-
 cp ${TODAY_DIR}/12-00*.jpg ${YEAR_TIMELAPSE}/${DATE_DIR}.jpg
 chmod a+rx ${YEAR_TIMELAPSE}/${DATE_DIR}.jpg
+
+# Copy snapshots for each hour
+HOURLY_FILES=$(find ${BASE_DIR}/${YEAR}-* -name "*00-00.jpg")
+#echo $HOURLY_FILES
+for HOURLY_FILE in $HOURLY_FILES; do
+    FIXED_DIR=${HOURLY_FILE/$BASE_DIR\//}
+    FIXED_NAME=${HOURLY_DIR}/${FIXED_DIR/\//-}
+    echo "Copying ${HOURLY_FILE} to ${FIXED_NAME}"
+    cp ${HOURLY_FILE} ${FIXED_NAME}
+done
+
+# Do the daily snaps first
+rm -f $TIMELAPSE_YEAR
+cat ${YEAR_TIMELAPSE}/*.jpg | ffmpeg -framerate 5  \
+       -f image2pipe \
+       -vcodec mjpeg \
+       -i -          \
+       -vcodec mpeg4 \
+       -b:v 9999999  \
+       -qscale:v 0.1 \
+       -f avi $TIMELAPSE_YEAR
+chmod a+rx $TIMELAPSE_YEAR
+
+# Do the hourly snaps next
+rm -f $TIMELAPSE_HOURLY
+cat ${HOURLY_DIR}/*.jpg | ffmpeg -framerate 24  \
+       -f image2pipe \
+       -vcodec mjpeg \
+       -i -          \
+       -vcodec mpeg4 \
+       -b:v 9999999  \
+       -qscale:v 0.1 \
+       -f avi $TIMELAPSE_HOURLY
+chmod a+rx $TIMELAPSE_HOURLY
 
 #set -e
 rm -f $TIMELAPSE_FILE
@@ -60,17 +100,3 @@ cat $FILES | ffmpeg -framerate 10  \
 
 chmod a+rx $TIMELAPSE_FILE
 
-#rm -rf ${TODAY_DIR}/*.jpg
-
-TIMELAPSE_YEAR=${YEAR_DIR}/$(date +'%Y')-timelapse.avi
-rm -f $TIMELAPSE_YEAR
-cat ${YEAR_TIMELAPSE}/*.jpg | ffmpeg -framerate 5  \
-       -f image2pipe \
-       -vcodec mjpeg \
-       -i -          \
-       -vcodec mpeg4 \
-       -b:v 9999999  \
-       -qscale:v 0.1 \
-       -f avi $TIMELAPSE_YEAR
-
-chmod a+rx $TIMELAPSE_YEAR
