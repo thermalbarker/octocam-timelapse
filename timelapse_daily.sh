448097de97477914954e9f41d2769469e46da35e
Thomas Barber <thermalbarker@gmail.com>
2020-03-29 11:53:07 +0200

    Make timelapse for daylight hours

--- timelapse_daily.sh
+++ timelapse_daily.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 BASE_DIR=/data/output/Camera1
 OUTPUT_DIR=/data/output/timelapse
@@ -9,6 +10,9 @@ YEAR_DIR="${OUTPUT_DIR}/${YEAR}"
 YEAR_TIMELAPSE=${YEAR_DIR}/stills
 
 HOURLY_DIR=${YEAR_DIR}/hourly
+
+# Clean up old files
+find ${YEAR_DIR} -type f -mtime +7 -name '*.avi' -exec rm -- '{}' \;
 
 TIMELAPSE_NAME=${DATE_DIR}-timelapse.avi
 TIMELAPSE_FILE="${YEAR_DIR}/${TIMELAPSE_NAME}"
@@ -48,11 +52,11 @@ FILES=$(find $TODAY_DIR -name '*.jpg' $SUNRISE $SUNSET | sort)
 #echo $FILES
 
 # Copy the snapshot from midday
-cp ${TODAY_DIR}/12-00*.jpg ${YEAR_TIMELAPSE}/${DATE_DIR}.jpg
-chmod a+rx ${YEAR_TIMELAPSE}/${DATE_DIR}.jpg
+cp ${TODAY_DIR}/12-00*.jpg ${YEAR_TIMELAPSE}/${DATE_DIR}.jpg || true
+chmod a+rx ${YEAR_TIMELAPSE}/${DATE_DIR}.jpg || true
 
 # Copy snapshots for each hour
-HOURLY_FILES=$(find ${BASE_DIR}/${YEAR}-* -name "*00-00.jpg")
+HOURLY_FILES=$(find ${BASE_DIR}/${YEAR}-* -name "*-00-00.jpg")
 #echo $HOURLY_FILES
 for HOURLY_FILE in $HOURLY_FILES; do
     FIXED_DIR=${HOURLY_FILE/$BASE_DIR\//}
@@ -60,6 +64,27 @@ for HOURLY_FILE in $HOURLY_FILES; do
     echo "Copying ${HOURLY_FILE} to ${FIXED_NAME}"
     cp ${HOURLY_FILE} ${FIXED_NAME}
 done
+
+# Remove photos which were taken in the dark
+# TODO: find a better way to do this!
+rm ${HOURLY_DIR}/*-*-*-00-00-00.jpg
+rm ${HOURLY_DIR}/*-*-*-01-00-00.jpg
+rm ${HOURLY_DIR}/*-*-*-02-00-00.jpg
+rm ${HOURLY_DIR}/*-*-*-03-00-00.jpg
+rm ${HOURLY_DIR}/*-*-*-04-00-00.jpg
+rm ${HOURLY_DIR}/*-*-*-05-00-00.jpg
+rm ${HOURLY_DIR}/*-*-*-06-00-00.jpg
+rm ${HOURLY_DIR}/*-*-*-07-00-00.jpg
+#rm ${HOURLY_DIR}/*-*-*-08-00-00.jpg
+# It is probably light from 0800 to 1600
+#rm ${HOURLY_DIR}/*-*-*-16-00-00.jpg
+rm ${HOURLY_DIR}/*-*-*-17-00-00.jpg
+rm ${HOURLY_DIR}/*-*-*-18-00-00.jpg
+rm ${HOURLY_DIR}/*-*-*-19-00-00.jpg
+rm ${HOURLY_DIR}/*-*-*-20-00-00.jpg
+rm ${HOURLY_DIR}/*-*-*-21-00-00.jpg
+rm ${HOURLY_DIR}/*-*-*-22-00-00.jpg
+rm ${HOURLY_DIR}/*-*-*-23-00-00.jpg
 
 # Do the daily snaps first
 rm -f $TIMELAPSE_YEAR
